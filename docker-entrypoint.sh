0e5382d61224e3f4daa9bdaee5f36f375eca7a82
johnerikhalse <johnerikhalse@gmail.com>
2018-02-12 10:46:48 +0100

    initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# ----------------------------------------------------------------------------
+# entrypoint for certificate generator
+# ----------------------------------------------------------------------------
+set -e
+
+openssl ecparam -name prime256v1 -out /ca-certificates/ec.param
+openssl req -new -newkey ec:/ca-certificates/ec.param -days 365 -nodes -x509 -sha384 \
+    -config openssl.conf \
+    -keyout /ca-certificates/cache-selfsigned.key \
+    -out /ca-certificates/cache-selfsignedCA.crt \
+    -subj "/O=Veidemann harvester/OU=Veidemann cache/CN=veidemann-harvester"
+chmod -R 777 /ca-certificates
